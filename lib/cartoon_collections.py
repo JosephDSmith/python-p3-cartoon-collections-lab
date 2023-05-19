2bd259266a8794fb7306b607e3d996ad5efe87e4
Joseph Smith <joesmith40@gmail.com>
2023-05-19 16:43:27 -0500

    completed lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,33 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    numbers = range(1, len(dwarves) + 1)
+    for number in numbers:
+        print(f'{number}. {dwarves[number-1]}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    list = []
+    for call in planeteer_calls:
+        list.append(f'{call[0].upper()}{call[1:]}!')
+    return list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    tf_list = []
+    for word in words:
+        if len(word) > 4:
+            tf_list.append("True")
+        else:
+            tf_list.append("False")
+    if "True" in tf_list:
+        return True
+    else:
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    for food in foods:
+        if food == "cheddar":
+            return "cheddar"
+        elif food == "gouda":
+            return "gouda"
+        elif food == "camembert":
+            return "camembert"
+        
+    return None
